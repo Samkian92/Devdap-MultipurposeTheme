2198a4b9c3ac0160a02c17dade0644c2dee42906
Sam <samkian92@gmail.com>
2019-04-23 10:45:39 +0800

    Changed Homepage to coming-soon page

--- Devdap-Core/devdap_core/devdap_core/Startup.cs
+++ Devdap-Core/devdap_core/devdap_core/Startup.cs
@@ -43,7 +43,7 @@ namespace devdap_core
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Empty}/{action=coming_soon}/{id?}");
             });
         }
     }
